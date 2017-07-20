6a7de7f7f7a1d7be5e87a5733c0b2942082fb8de
Dan Strong <danielstrong@mac.com>
2017-07-20 20:32:33 +0000

    add dynamic board display functionality

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,46 @@
-var board = [ 
-    [' ', ' ', ' '], 
-    [' ', ' ', ' '], 
-    [' ', ' ', ' '] 
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
+};
 
-const printBoard = board => {
-    console.log('Current Board:')
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
-}
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced <= numberOfBombs) { 
+        // This code has the potential to place bombs on top of already existing bombs
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced += 1;
+    }
+    
+    return board;
+};
 
+const printBoard = (board) => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
 
-printBoard(board);
-
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+console.log(playerBoard);
+console.log(bombBoard);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
